91c594b3975e4e92131dee5ed636bcf7be66bf5d
Stefan Petrov <102225920+stefpetrov@users.noreply.github.com>
2022-03-28 09:52:31 +0300

    Add files via upload

---
+++ First Steps in Coding/weatherForecast.js
@@ -0,0 +1,15 @@
+function weatherForecast(input) {
+
+    if (input[0] ==  "sunny") {
+        console.log("It's warm outside!");
+    } else {
+        console.log("It's cold outside!");
+    }
+
+    
+}
+
+weatherForecast(["sunny"]);
+
+
+
