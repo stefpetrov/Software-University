5533f5013f3e71a24db7a612dc26bd2729052a59
Stefan Petrov <102225920+stefpetrov@users.noreply.github.com>
2022-03-28 09:52:01 +0300

    Add files via upload

---
+++ First Steps in Coding/triangleArea.js
@@ -0,0 +1,13 @@
+function triangleArea(input){
+    let a = Number(input[0]);
+    let h = Number(input[1]);
+
+    let area = a * h / 2
+
+    console.log(area.toFixed(2));
+
+
+}
+
+triangleArea([8, 13, 7]);
+
