4aacc9ec492e0723af1d1ee2544d5993df889dd0
Stefan Petrov <102225920+stefpetrov@users.noreply.github.com>
2022-03-31 12:34:15 +0300

    Add files via upload

---
+++ Conditional Statements - Advanced/12tradeCommissions.js
@@ -0,0 +1,50 @@
+function tradeCommissions(input) {
+
+    let town = input[0];
+    let sales = Number(input[1]);
+
+    switch(town) {
+        case "Sofia":
+            if(sales >= 0 && sales <= 500) {
+                console.log((sales * 0.05).toFixed(2))
+            } else if(sales > 500 && sales <= 1000) {
+                console.log((sales * 0.07).toFixed(2))
+            } else if(sales > 1000 && sales <= 10000) {
+                console.log((sales * 0.08).toFixed(2))
+            } else if(sales > 10000) {
+                console.log((sales * 0.12).toFixed(2))
+            } else {
+                console.log("error")
+            } break;
+        case "Varna":
+            if(sales >= 0 && sales <= 500) {
+                console.log((sales * 0.045).toFixed(2))
+            } else if(sales > 500 && sales <= 1000) {
+                console.log((sales * 0.075).toFixed(2))
+            } else if(sales > 1000 && sales <= 10000) {
+                console.log((sales * 0.10).toFixed(2))
+            } else if(sales > 10000) {
+                console.log((sales * 0.13).toFixed(2))
+            } else {
+                console.log("error")
+            } break;
+        case "Plovdiv":
+            if(sales >= 0 && sales <= 500) {
+                console.log((sales * 0.055).toFixed(2))
+            } else if(sales > 500 && sales <= 1000) {
+                console.log((sales * 0.08).toFixed(2))
+            } else if(sales > 1000 && sales <= 10000) {
+                console.log((sales * 0.12).toFixed(2))
+            } else if(sales > 10000) {
+                console.log((sales * 0.145).toFixed(2))
+            } else {
+                console.log("error")
+            } break;
+            default: 
+            console.log("error")
+
+    }
+
+}
+tradeCommissions(["Kaspichan",
+"-50"])
