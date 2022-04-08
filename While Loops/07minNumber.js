f8c2f8e276809399146b8be1cc7f08af7309f269
Stefan Petrov <102225920+stefpetrov@users.noreply.github.com>
2022-04-08 10:49:57 +0300

    Add files via upload

---
+++ While Loops/07minNumber.js
@@ -0,0 +1,22 @@
+function minNumber(input) {
+  let index = 0;
+  let command = input[index];
+  index++;
+
+  let minNum = Number.MAX_SAFE_INTEGER;
+
+  while (command !== "Stop") {
+    let currentNum = Number(command);
+
+    if (currentNum < minNum) {
+      minNum = currentNum;
+    }
+
+    command = input[index];
+    index++;
+  }
+
+  console.log(minNum);
+}
+
+minNumber(["-10", "20", "-30", "Stop"]);
