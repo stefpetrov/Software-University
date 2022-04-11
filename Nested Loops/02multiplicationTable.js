d9e8c4824274fc2675219decddeb654b04e9d7c2
Stefan Petrov <102225920+stefpetrov@users.noreply.github.com>
2022-04-11 10:17:35 +0300

    Add files via upload

---
+++ Nested Loops/02multiplicationTable.js
@@ -0,0 +1,22 @@
+function multiplicationTable() {
+
+
+    for(let x = 1; x <= 10; x++){
+
+
+        for(let y = 1; y <= 10; y++){
+
+             x = Number(x);
+             y = Number(y);
+
+            console.log(`${x} * ${y} = ${x * y}`)
+        }
+    }
+
+
+
+
+
+}
+
+multiplicationTable()
