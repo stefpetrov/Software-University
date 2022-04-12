bd5c98dd56a36fd1fffe5a349277af4326996c39
Stefan Petrov <102225920+stefpetrov@users.noreply.github.com>
2022-04-12 10:19:19 +0300

    Add files via upload

---
+++ Nested Loops/05specialNumbers.js
@@ -0,0 +1,27 @@
+function specialNumbers(input){
+
+
+    let n = Number(input[0]);
+    let result = ""
+    for(let a = 1; a <= 9; a++){
+
+        for(b = 1; b <= 9; b++){
+            for(c = 1; c <= 9; c++ ){
+                for(d = 1; d <= 9; d++){
+
+                    if(n % a === 0 && n % b === 0 && n % c === 0 && n % d === 0){
+
+                        result = result + a + b + c + d + " "
+
+                        
+                        
+                    }
+                }
+            }
+        }
+    }
+    console.log(result)
+
+}
+
+specialNumbers(["3"])
