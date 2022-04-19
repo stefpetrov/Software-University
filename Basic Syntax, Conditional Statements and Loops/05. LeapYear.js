ecaf0743e5720ae5cd4c4343776335d12eb819ab
Stefan Petrov <102225920+stefpetrov@users.noreply.github.com>
2022-04-19 10:42:44 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops/05. LeapYear.js
@@ -0,0 +1,14 @@
+function solve(y){
+    let year = Number(y);
+
+    if((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0){
+        console.log("yes")
+    }
+    else{
+        console.log("no")
+    }
+
+
+}
+
+solve(4)
