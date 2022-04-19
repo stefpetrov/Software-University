2236de708c85b988137347c4c9bd255c2139b66f
Stefan Petrov <102225920+stefpetrov@users.noreply.github.com>
2022-04-19 10:43:15 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops/07. TriangleOfNumbers.js
@@ -0,0 +1,12 @@
+function solve(n) {
+  let endLine = Number(n);
+
+  for (let i = 1; i <= endLine; i++) {
+    let output = "";
+    for (let j = 1; j <= i; j++) {
+      output += i + " ";
+    }
+    console.log(output);
+  }
+}
+solve(3);
