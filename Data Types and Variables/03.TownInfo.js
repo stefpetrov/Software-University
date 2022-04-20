638ed2a32a457fcbfa478fc80f8af3c5e37d36a0
Stefan Petrov <102225920+stefpetrov@users.noreply.github.com>
2022-04-20 11:35:18 +0300

    Add files via upload

---
+++ Data Types and Variables/03.TownInfo.js
@@ -0,0 +1,8 @@
+function solve(town, population, area) {
+
+    let output =(`Town ${town} has population of ${population} and area ${area} square km.`)
+
+    console.log(output)
+}
+
+solve('Sofia',1286383,492)
