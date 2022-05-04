1d97cc7c0b11627aebbdd3167a71dbb67d2b870b
Stefan Petrov <102225920+stefpetrov@users.noreply.github.com>
2022-05-04 11:11:35 +0300

    Add files via upload

---
+++ Mid Exam-Tasks/5- 02.MuOnline.js
@@ -0,0 +1,56 @@
+function muOnline(arr) {
+  let rooms = arr.split("|");
+
+  let health = 100;
+  let coins = 0;
+  let counter = 0;
+
+  let healthPlus = 0;
+  let coinsPlus = 0;
+  let monsterDemage = 0;
+
+  let isPassed = true;
+
+  for (let i = 0; i < rooms.length; i++) {
+    let command = rooms[i].split(" ").shift();
+    counter++;
+
+    if (command === "potion") {
+      healthPlus = Number(rooms[i].split(" ").pop());
+      health += healthPlus;
+      let usedPotion = healthPlus;
+      if (health > 100) {
+        usedPotion = 100 - (health - healthPlus);
+        health = 100;
+      }
+
+      console.log(`You healed for ${usedPotion} hp.`);
+      console.log(`Current health: ${health} hp.`);
+    } else if (command === "chest") {
+      coinsPlus = Number(rooms[i].split(" ").pop());
+      coins += coinsPlus;
+      console.log(`You found ${coinsPlus} bitcoins.`);
+    } else {
+      monsterName = rooms[i].split(" ").shift();
+      monsterDemage = Number(rooms[i].split(" ").pop());
+      health -= monsterDemage;
+      if (health > 0) {
+        console.log(`You slayed ${monsterName}.`);
+      } else {
+        console.log(`You died! Killed by ${monsterName}.`);
+        isPassed = false;
+
+        break;
+      }
+    }
+  }
+  if (isPassed) {
+    console.log("You've made it!");
+    console.log(`Bitcoins: ${coins}`);
+    console.log(`Health: ${health}`);
+  } else {
+    console.log(`Best room: ${counter}`);
+  }
+}
+
+muOnline("cat 10|potion 30|orc 10|chest 10|snake 25|chest 110");
