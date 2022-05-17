4ae536f4b24eff426abbb7e2996f373708424f6c
Stefan Petrov <102225920+stefpetrov@users.noreply.github.com>
2022-05-17 16:06:27 +0300

    Add files via upload

---
+++ Text Processing/02. Substring.js
@@ -0,0 +1,10 @@
+function solve(str,startIndex,countElements) {
+    let endIndex = startIndex + countElements
+    let modifiedString = str.substring(startIndex,endIndex)
+
+    console.log(modifiedString)
+
+
+}
+
+solve('ASentence', 1, 8)
