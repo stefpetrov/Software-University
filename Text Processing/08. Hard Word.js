da2a2005083a806dc22d0d975ca5afd2b1e12b60
Stefan Petrov <102225920+stefpetrov@users.noreply.github.com>
2022-05-18 09:59:40 +0300

    Add files via upload

---
+++ Text Processing/08. Hard Word.js
@@ -0,0 +1,28 @@
+function solve(input) {
+  let text = input.shift();
+  let arrOfwords = input.shift();
+
+  let splittedText = text.split(" ");
+  for (let potentialHole of splittedText) {
+    for (let word of arrOfwords) {
+      let wordL = word.length;
+      if (potentialHole.includes("_")) {
+        let hole = potentialHole;
+
+        let lastIndex = hole.lastIndexOf("_");
+        let holeL = lastIndex + 1;
+        let holeToFill = "_".repeat(holeL);
+        if (holeL === wordL) {
+          text = text.replace(holeToFill, word);
+        }
+      }
+    }
+  }
+
+  console.log(text);
+}
+
+solve([
+  "Hi, grandma! I'm so ____ to write to you. ______ the winter vacation, so _______ things happened. My dad bought me a sled. Mom started a new job as a __________. My brother's ankle is ________, and now it bothers me even more. Every night Mom cooks ___ on your recipe because it is the most delicious. I hope this year Santa will _____ me a robot.",
+  ["pie", "bring", "glad", "During", "amazing", "pharmacist", "sprained"],
+]);
